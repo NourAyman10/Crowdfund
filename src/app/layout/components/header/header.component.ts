744e480000905719817641ee611c3df33375bfb0
Nourhan Ayman <83882344+Nour10102002@users.noreply.github.com>
2025-08-14 19:21:05 +0300

    finish header

--- src/app/layout/components/header/header.component.ts
+++ src/app/layout/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { ButtonComponent } from '../../../components/shared/button/button.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, ButtonComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
