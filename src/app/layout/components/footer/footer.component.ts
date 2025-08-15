2b68fbf6f4db75c1079117477a23c2b92ebf7aab
Nourhan Ayman <83882344+Nour10102002@users.noreply.github.com>
2025-08-15 10:24:27 +0300

    finish footer first section

--- src/app/layout/components/footer/footer.component.ts
+++ src/app/layout/components/footer/footer.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { NotifiedSectionComponent } from './notified-section/notified-section.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [NotifiedSectionComponent],
+  imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
-export class FooterComponent {
-
-}
+export class FooterComponent {}
--- src/app/layout/main-layout/main-layout.component.ts
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -2,14 +2,18 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from '../components/footer/footer.component';
 import { HeaderComponent } from '../components/header/header.component';
+import { NewsletterCTASectionComponent } from '../../components/shared/newsletter-cta-section/newsletter-cta-section.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    NewsletterCTASectionComponent,
+    FooterComponent,
+    HeaderComponent,
+  ],
   templateUrl: './main-layout.component.html',
-  styleUrl: './main-layout.component.scss'
+  styleUrl: './main-layout.component.scss',
 })
-export class MainLayoutComponent {
-
-}
+export class MainLayoutComponent {}
